edfa2ce044c6e603be11485523192d633e427c9d
marcushanses <79835961+marcushanses@users.noreply.github.com>
2022-11-12 21:19:37 +0100

    Support for string literals. (#3)
    Co-authored-by: Marcus Hanses <marcus.hanses@gmail.com>

--- String.ts
+++ String.ts
@@ -2,12 +2,16 @@ import { Type } from "./Type"
 
 class StringClass extends Type<string> {
 	readonly name = "string"
-	constructor() {
+	constructor(readonly strings?: string[]) {
 		super()
 	}
 	is(value: any | string): value is string {
-		return typeof value == "string"
+		return typeof value == "string" && (!this.strings || this.strings.includes(value))
 	}
 }
+
 export type String = StringClass
-export const string = new StringClass()
+
+export function string(...strings: string[]): StringClass {
+	return new StringClass(strings.length == 0 ? undefined : strings)
+}
