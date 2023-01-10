ce5d7e043895ecf5babb36d2d03532cd1b675b20
Lasse Löfquist <github@tvartom.com>
2023-01-10 13:05:55 +0100

    string() supports RegExp, and shows conditions.

--- string.spec.ts
+++ string.spec.ts
@@ -16,10 +16,24 @@ describe("string", () => {
 		expect(isly.string().flaw(42)).toEqual({ type: "string" })
 	})
 	it("literal", () => {
-		expect(isly.string("42").is("42")).toBeTruthy()
-		expect(isly.string("42").is("43")).toBeFalsy()
 		expect(isly.string("42", "43").is("43")).toBeTruthy()
 		expect(isly.string(...["42", "43"]).is("43")).toBeTruthy()
-		expect(isly.string("42").flaw(42)).toEqual({ type: "string" })
+
+		const string42Type = isly.string("42")
+
+		expect(string42Type.is("42")).toBeTruthy()
+		expect(string42Type.is("43")).toBeFalsy()
+		expect(string42Type.is("")).toBeFalsy()
+		expect(string42Type.flaw(42)).toEqual({ type: "string", condition: '"42"' })
+		expect(isly.string(...["42", "43"]).flaw("44")).toEqual({ type: "string", condition: '"42" | "43"' })
+	})
+	it("regexp", () => {
+		const stringRegexpType = isly.string(/^'.*'$/)
+		expect(stringRegexpType.is("'42'")).toBeTruthy()
+		expect(stringRegexpType.is("42")).toBeFalsy()
+
+		expect(stringRegexpType.flaw(42)).toEqual({ type: "string", condition: "/^'.*'$/" })
+
+		expect(isly.string(/^aaa$/i, /^BBB$/).flaw(undefined)).toEqual({ type: "string", condition: "/^aaa$/i | /^BBB$/" })
 	})
 })
