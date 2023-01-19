16111be3601e98880c06d3cb3b6fcf96901feedb
Lasse Löfquist <github@tvartom.com>
2023-01-19 10:45:37 +0100

    Type lazy() takes an optional name.

--- lazy.ts
+++ lazy.ts
@@ -1,9 +1,17 @@
 import { Type } from "./Type"
 
-export function lazy<T>(factory: () => Type<T>): Type<T> {
+/**
+ * Late evaluation of a type
+ * Can be used for for recursive types.
+ *
+ * @param factory
+ * @param name Provide a name, to avoid infinite loop if the type i recursive
+ * @returns
+ */
+export function lazy<T>(factory: () => Type<T>, name?: string): Type<T> {
 	let type: Type<T>
 	return Type.create(
-		() => (type ??= factory()).name,
+		name ?? (() => (type ??= factory()).name),
 		(value => (type ??= factory()).is(value)) as Type.IsFunction<T>,
 		(value => (type ??= factory()).flaw(value)) as Type.FlawFunction
 	)
