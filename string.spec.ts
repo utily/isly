f510186198abfe0850cbcea9fc77f14201c0a413
Lasse Löfquist <github@tvartom.com>
2023-01-19 10:47:03 +0100

    Type string() use a object for O(1) on long lists.

--- string.spec.ts
+++ string.spec.ts
@@ -4,11 +4,29 @@ import * as isly from "./index"
 describe("string", () => {
 	// TypeScript compile error if not working
 	it("TypeScript narrowing", () => {
-		const stringType = isly.string()
-		const isNarrowingWorking: boolean | string | any = true as any
-		if (stringType.is(isNarrowingWorking)) {
-			// eslint-disable-next-line @typescript-eslint/no-unused-vars
-			const myString: string = isNarrowingWorking
+		{
+			const stringType = isly.string()
+			const isNarrowingWorking: boolean | string | any = true as any
+			if (stringType.is(isNarrowingWorking)) {
+				// eslint-disable-next-line @typescript-eslint/no-unused-vars
+				const myString: string = isNarrowingWorking
+			}
+		}
+		{
+			const stringType = isly.string(["A", "B"])
+			const isNarrowingWorking: boolean | string | any = true as any
+			if (stringType.is(isNarrowingWorking)) {
+				// eslint-disable-next-line @typescript-eslint/no-unused-vars
+				const myString: "A" | "B" = isNarrowingWorking
+			}
+		}
+		{
+			const stringType = isly.string({ A: true, B: true })
+			const isNarrowingWorking: boolean | string | any = true as any
+			if (stringType.is(isNarrowingWorking)) {
+				// eslint-disable-next-line @typescript-eslint/no-unused-vars
+				const myString: "A" | "B" = isNarrowingWorking
+			}
 		}
 	})
 	it("generic", () => {
@@ -16,8 +34,7 @@ describe("string", () => {
 		expect(isly.string().flaw(42)).toEqual({ type: "string" })
 	})
 	it("literal", () => {
-		expect(isly.string("42", "43").is("43")).toBeTruthy()
-		expect(isly.string(...["42", "43"]).is("43")).toBeTruthy()
+		expect(isly.string(["42", "43"]).is("43")).toBeTruthy()
 
 		const string42Type = isly.string("42")
 
@@ -25,7 +42,7 @@ describe("string", () => {
 		expect(string42Type.is("43")).toBeFalsy()
 		expect(string42Type.is("")).toBeFalsy()
 		expect(string42Type.flaw(42)).toEqual({ type: "string", condition: '"42"' })
-		expect(isly.string(...["42", "43"]).flaw("44")).toEqual({ type: "string", condition: '"42" | "43"' })
+		expect(isly.string(["42", "43"]).flaw("44")).toEqual({ type: "string", condition: '"42" | "43"' })
 	})
 	it("regexp", () => {
 		const stringRegexpType = isly.string(/^'.*'$/)
@@ -33,7 +50,5 @@ describe("string", () => {
 		expect(stringRegexpType.is("42")).toBeFalsy()
 
 		expect(stringRegexpType.flaw(42)).toEqual({ type: "string", condition: "/^'.*'$/" })
-
-		expect(isly.string(/^aaa$/i, /^BBB$/).flaw(undefined)).toEqual({ type: "string", condition: "/^aaa$/i | /^BBB$/" })
 	})
 })
