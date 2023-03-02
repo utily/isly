ff21daa5afb1d8b74681ea164ddf28f01ef9553b
Lasse Löfquist <github@tvartom.com>
2023-03-02 09:38:33 +0100

    any() defaults to Type<any> (Not Type<unknown>).

--- any.spec.ts
+++ any.spec.ts
@@ -2,6 +2,16 @@ import "jest"
 import * as isly from "./index"
 
 describe("isly.any", () => {
+	// TypeScript compile error if not working
+	it("TypeScript narrowing", () => {
+		const anyType = isly.any()
+
+		const isNarrowingWorking: boolean | string | any = "garbage" as any
+		if (anyType.is(isNarrowingWorking)) {
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			const myArray: { a: 1 } = isNarrowingWorking
+		}
+	})
 	it("generic", () => {
 		const anyType = isly.any()
 
--- any.ts
+++ any.ts
@@ -1,9 +1,9 @@
 import { Type } from "./Type"
 
-class IslyAny<T> extends Type.AbstractType<T> {
+class IslyAny<T = any> extends Type.AbstractType<T> {
 	is = (value => value != undefined) as Type.IsFunction<T>
 }
 
-export function any<T>(name?: string): Type<T> {
+export function any<T = any>(name?: string): Type<T> {
 	return new IslyAny<T>(name ?? "any")
 }
