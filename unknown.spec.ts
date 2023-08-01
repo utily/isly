1bb0e34bf737105310be46f4b85b1be71c86c1c5
Elias Eriksson <eliaseriksson95@gmail.com>
2023-08-01 14:35:50 +0200

    added undefined and unknown (#16)
    * added undefined and unknown
    
    * get tests

---
+++ unknown.spec.ts
@@ -0,0 +1,29 @@
+import { isly } from "./index"
+
+describe("isly.unknown", () => {
+	it("TypeScript narrowing", () => {
+		const unknown = isly.unknown()
+
+		const narrowingUnknown = "garbage" as unknown
+		if (unknown.is(narrowingUnknown)) {
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			const t: unknown = narrowingUnknown
+		}
+		const narrowingAny = "garbage" as any
+		if (unknown.is(narrowingAny)) {
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			const t: unknown = narrowingAny
+		}
+	})
+	it("generic", () => {
+		const unknown = isly.unknown()
+		expect(unknown.is("42")).toEqual(true)
+		expect(unknown.is(null)).toEqual(false)
+		expect(unknown.is(undefined)).toEqual(false)
+		expect(unknown.flaw(undefined)).toEqual({ type: "unknown" })
+	})
+	it("get", () => {
+		const unknown = isly.unknown()
+		expect(unknown.get(42)).toEqual(42)
+	})
+})
