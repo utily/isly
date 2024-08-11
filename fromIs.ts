73861ddb1c1eed9650fd6ca2bfe3c8174693d400
Marcus Hanses <marcus.hanses@pax2pay.com>
2024-08-11 22:04:11 +0200

    Fixed fromIs type.

--- fromIs.ts
+++ fromIs.ts
@@ -12,8 +12,8 @@ class IslyFromIs<T> extends Type<T> {
  *
  * @param config
  */
-export function fromIs<T>(name: string, is: Type<T>["is"]): Type<T>
+export function fromIs<T>(name: string, is: (value: T | any) => value is T): Type<T>
 export function fromIs<T>(name: string, is: (value: any) => boolean): Type<T>
-export function fromIs<T>(name: string, is: (value: any) => boolean): Type<T> {
+export function fromIs<T>(name: string, is: ((value: T | any) => value is T) | ((value: any) => boolean)): Type<T> {
 	return new IslyFromIs(name, is)
 }
