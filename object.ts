8bd950cfe0276a98f8a517b02523a7391f5acec6
marcushanses <79835961+marcushanses@users.noreply.github.com>
2024-08-16 14:13:44 +0200

    Updated object types. (#27)

--- object.ts
+++ object.ts
@@ -1,37 +1,17 @@
 import type { Flaw } from "./Flaw"
 import { Type } from "./Type"
 
-type OptionalKeys<T> = NonNullable<{ [K in keyof T]: undefined extends T[K] ? K : never }[keyof T]>
-type RequiredKeys<T> = NonNullable<{ [K in keyof T]: undefined extends T[K] ? never : K }[keyof T]>
-
 export namespace object {
-	export type BaseProperties<T> = { [P in OptionalKeys<T>]: Type<T[P] | undefined> } &
-		{
-			[P in RequiredKeys<T>]: Type<T[P]>
-		}
-	export type ExtendedProperties<T2 extends T, T> = BaseProperties<Omit<T2, keyof T>> &
-		Partial<object.BaseProperties<Pick<T2, keyof T>>>
-	export type Properties<T extends B, B, TB extends Type<B> | undefined> = TB extends undefined
-		? object.BaseProperties<T>
-		: object.ExtendedProperties<T, B>
-
-	export interface ExtendableType<T> extends Type<T> {
-		extend<T2 extends T>(properties: ExtendedProperties<T2, T>, name?: string): ExtendableType<T2>
-		omit<K extends keyof T>(omits: readonly K[], name?: string): object.ExtendableType<Omit<T, K>>
-		omit<T2 extends Omit<T, K>, K extends keyof T>(omits: readonly K[], name?: string): object.ExtendableType<T2>
-		pick<K extends keyof T>(picks: readonly K[], name?: string): object.ExtendableType<Pick<T, K>>
-		pick<T2 extends Pick<T, K>, K extends keyof T>(picks: readonly K[], name?: string): object.ExtendableType<T2>
-	}
+	type TypedProperties<T> = { [P in keyof T]: Type<T[P]> }
+	export type Properties<T extends B, B extends object = object> = Required<TypedProperties<Omit<T, keyof B>>> &
+		Partial<TypedProperties<Pick<T, keyof B>>>
 }
 
-class IslyObject<T extends B, B extends object, TB extends IslyObject<B, any, any> | undefined>
-	extends Type<T>
-	implements object.ExtendableType<T>
-{
+class IslyObject<T extends B, B extends object = object> extends Type<T> {
 	constructor(
-		protected readonly baseType: TB,
-		protected readonly properties: object.Properties<T, B, TB>,
-		name?: string
+		protected readonly properties: object.Properties<T, B>,
+		name?: string,
+		protected readonly baseType?: IslyObject<B, any>
 	) {
 		super(
 			() =>
@@ -42,32 +22,25 @@ class IslyObject<T extends B, B extends object, TB extends IslyObject<B, any, an
 						.join(", ")}}`
 		)
 	}
-	extend<T2 extends T>(
-		properties: { [P in OptionalKeys<Omit<T2, keyof T>>]: Type<Omit<T2, keyof T>[P] | undefined> } &
-			{
-				[P in RequiredKeys<Omit<T2, keyof T>>]: Type<Omit<T2, keyof T>[P]>
-			} &
-			Partial<object.BaseProperties<Pick<T2, keyof T>>>,
-		name?: string | undefined
-	): object.ExtendableType<T2> {
-		return new IslyObject<T2, T, IslyObject<T, any, any>>(this, properties, name)
+	extend<T2 extends T>(properties: object.Properties<T2, T>, name?: string | undefined): IslyObject<T2, T> {
+		return new IslyObject<T2, T>(properties, name, this)
 	}
-	omit<K extends keyof T>(omits: readonly K[], name?: string): object.ExtendableType<Omit<T, K>> {
-		return new IslyObject<Omit<T, K>, Omit<T, K>, any>(
-			this.baseType?.omit(omits as any),
+	omit<K extends keyof T>(omits: readonly K[], name?: string): IslyObject<Omit<T, K>> {
+		return new IslyObject<Omit<T, K>>(
 			globalThis.Object.fromEntries(
 				globalThis.Object.entries(this.properties).filter(([key]) => !omits.includes(key as any))
 			) as any,
-			name ?? `Omit<${this.name}, ${omits.map(key => `"${String(key)}"`).join(" | ")}>`
+			name ?? `Omit<${this.name}, ${omits.map(key => `"${String(key)}"`).join(" | ")}>`,
+			this.baseType?.omit(omits as any)
 		)
 	}
-	pick<K extends keyof T>(picks: readonly K[], name?: string): object.ExtendableType<Pick<T, K>> {
-		return new IslyObject<Pick<T, K>, Pick<T, K>, any>(
-			this.baseType?.pick(picks as any),
+	pick<K extends keyof T>(picks: readonly K[], name?: string): IslyObject<Pick<T, K>> {
+		return new IslyObject<Pick<T, K>>(
 			globalThis.Object.fromEntries(
 				globalThis.Object.entries(this.properties).filter(([key]) => picks.includes(key as any))
 			) as any,
-			name ?? `Pick<${this.name}, ${picks.map(key => `"${String(key)}"`).join(" | ")}>`
+			name ?? `Pick<${this.name}, ${picks.map(key => `"${String(key)}"`).join(" | ")}>`,
+			this.baseType?.pick(picks as any)
 		)
 	}
 	is = (value: T | any): value is T =>
@@ -105,15 +78,8 @@ class IslyObject<T extends B, B extends object, TB extends IslyObject<B, any, an
 	}
 }
 
-export function object(): object.ExtendableType<object>
-export function object<T extends object>(properties: object.BaseProperties<T>, name?: string): object.ExtendableType<T>
-export function object<T extends object>(
-	properties?: object.BaseProperties<T>,
-	name?: string
-): object.ExtendableType<T> {
-	return new IslyObject<T, object, undefined>(
-		undefined,
-		properties ?? ({} as object.Properties<T, object, undefined>),
-		name
-	)
+export function object(): IslyObject<object>
+export function object<T extends object>(properties: object.Properties<T>, name?: string): IslyObject<T>
+export function object<T extends object>(properties?: object.Properties<T>, name?: string): IslyObject<T> {
+	return new IslyObject<T>(properties ?? ({} as object.Properties<T>), name)
 }
