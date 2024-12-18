709633f612edc8cdcd5246a57a5af6f22b9d6f5b
Simon Mika <simon@mika.se>
2024-12-18 20:20:13 +0100

    Fixed linting.

--- function.ts
+++ function.ts
@@ -1,10 +1,10 @@
 import { Type } from "./Type"
 
-// eslint-disable-next-line @typescript-eslint/ban-types
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export function islyFunction<T extends Function>(): Type<T> {
 	return new IslyFunction()
 }
-// eslint-disable-next-line @typescript-eslint/ban-types
+// eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
 export class IslyFunction<T extends Function = Function> extends Type<T> {
 	readonly class = "function"
 	constructor() {
