e356be189be10f18a786f206a5ce6bbff7e2261f
Simon Mika <simonmika@users.noreply.github.com>
2024-12-20 16:35:48 +0100

    String predicate (#32)
    * Improved configuration.
    
    * Added null type.
    
    * Added string predicate argument and descriptor.

---
+++ null.ts
@@ -0,0 +1,9 @@
+import { Type } from "./Type"
+
+export function islyNull<T = null>(name?: string): Type<T> {
+	return new IslyNull<T>(name ?? "null")
+}
+export class IslyNull<T = undefined> extends Type<T> {
+	readonly class = "null"
+	is = (value: T | any): value is T => value === null
+}
--- string.ts
+++ string.ts
@@ -3,26 +3,43 @@ import { Type } from "./Type"
 // TODO: Use `const` with Typescript 5
 // https://devblogs.microsoft.com/typescript/announcing-typescript-5-0-beta/#const-type-parameters
 /**
- * Then using RegExp, don't use the /g-flag. (It makes .test() statefull)
+ * Then using RegExp, don't use the /g-flag. (It makes .test() stateful)
  * @param condition
  */
 export function string<T extends string>(condition?: T): Type<T>
-export function string<T extends string>(condition?: readonly T[] | Record<T, any> | RegExp): Type<T>
-export function string<T extends string>(condition?: readonly T[] | Record<T, any> | RegExp | string): Type<T> {
-	return new IslyString<T>(condition)
+export function string<T extends string>(
+	condition?: readonly T[] | Record<T, any> | RegExp,
+	descriptor?: string
+): Type<T>
+export function string<T extends string>(condition: (value: string) => boolean, descriptor: string): Type<T>
+export function string<T extends string>(
+	condition?: readonly T[] | Record<T, any> | RegExp | ((value: string) => boolean) | string,
+	descriptor?: string
+): Type<T> {
+	return new IslyString<T>(condition, descriptor)
 }
 export class IslyString<T extends string = string> extends Type<T> {
 	readonly class = "string"
-	protected conditionObject: Record<T, any> | RegExp | true | undefined
-	constructor(readonly stringCondition?: readonly T[] | Record<T, any> | RegExp | string) {
-		super(typeof stringCondition == "string" ? `"${stringCondition}"` : "string", () => {
-			const conditionObject = this.getConditionObject()
-			return conditionObject instanceof RegExp
-				? `/${conditionObject.source}/${conditionObject.flags}`
-				: globalThis.Object.keys(conditionObject)
-						.map(key => `"${key}"`)
-						.join(" | ")
-		})
+	protected conditionObject: Record<T, any> | RegExp | ((value: string) => boolean) | true | undefined
+	constructor(
+		readonly stringCondition?: readonly T[] | Record<T, any> | RegExp | ((value: string) => boolean) | string,
+		readonly descriptor?: string
+	) {
+		super(
+			typeof stringCondition == "string" ? `"${stringCondition}"` : "string",
+			descriptor
+				? descriptor
+				: () => {
+						const conditionObject = this.getConditionObject()
+						return conditionObject instanceof RegExp
+							? `/${conditionObject.source}/${conditionObject.flags}`
+							: conditionObject instanceof Function
+							? "custom-predicate"
+							: globalThis.Object.keys(conditionObject)
+									.map(key => `"${key}"`)
+									.join(" | ")
+				  }
+		)
 	}
 	is = (value: T | any): value is T => {
 		const conditionObject = this.getConditionObject()
@@ -30,8 +47,10 @@ export class IslyString<T extends string = string> extends Type<T> {
 			typeof value == "string" &&
 			(conditionObject instanceof RegExp
 				? conditionObject.test(value)
+				: conditionObject instanceof Function
+				? conditionObject(value)
 				: typeof conditionObject == "object"
-				? value in conditionObject //TODO, avoid "hasOwnProperty toString etc."
+				? value in conditionObject // TODO, avoid "hasOwnProperty toString etc."
 				: conditionObject)
 		)
 	}
@@ -43,6 +62,8 @@ export class IslyString<T extends string = string> extends Type<T> {
 			  }, globalThis.Object.create(null))
 			: typeof this.stringCondition == "string"
 			? { [this.stringCondition]: true }
+			: typeof this.stringCondition == "function"
+			? this.stringCondition
 			: typeof this.stringCondition == "object" // Record or RegExp!
 			? this.stringCondition
 			: true)
