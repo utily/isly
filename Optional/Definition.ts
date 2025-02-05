4b3415bc8cc8899094368ea52b5ce24c1d5b3039
Simon Mika <simon@mika.se>
2025-02-05 18:09:52 +0100

    Updated configuration.

--- Optional/Definition.ts
+++ Optional/Definition.ts
@@ -1,5 +1,4 @@
 import { Base } from "../Base"
-import type { isly } from "../index"
 import type { Definition as islyDefinition } from "../Definition"
 
 export interface Definition extends Base.Definition {
