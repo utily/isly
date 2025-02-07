0ee227552e7321268a045d34a10ac9f907ff99ea
Simon Mika <simon@mika.se>
2025-02-07 21:57:55 +0100

    Improved Object naming.

--- Object/index.ts
+++ Object/index.ts
@@ -2,9 +2,9 @@ import { Class } from "./Class"
 import { Definition as ObjectDefinition } from "./Definition"
 import { Properties as ObjectProperties } from "./Properties"
 
-export type islyObject<V extends object = Record<string, any>> = Omit<Class<V>, "constructor">
+export type _Object<V extends object = Record<string, any>> = Omit<Class<V>, "constructor">
 
-export namespace islyObject {
+export namespace _Object {
 	export import Definition = ObjectDefinition
 	export import Properties = ObjectProperties
 }
