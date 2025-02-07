b50f972b7d92f8b1707b6c95cdae01bf85fb1d73
Simon Mika <simon@mika.se>
2025-02-07 21:55:13 +0100

    Correct export in place.

---
+++ creator.ts
@@ -0,0 +1,45 @@
+import { Class as AnyClass } from "Any/Class"
+import { Class as ArrayClass } from "Array/Class"
+import { Class as BooleanClass } from "Boolean/Class"
+import { Class as FromClass } from "From/Class"
+import { Class as FunctionClass } from "Function/Class"
+import { Class as InstanceClass } from "Instance/Class"
+import { Class as IntersectionClass } from "Intersection/Class"
+import { Class as NullClass } from "Null/Class"
+import { Class as NumberClass } from "Number/Class"
+import { Class as ObjectClass } from "Object/Class"
+import { Class as OptionalClass } from "Optional/Class"
+import { Class as ReadonlyClass } from "Readonly/Class"
+import { Class as RecordClass } from "Record/Class"
+import { Class as StringClass } from "String/Class"
+import { Class as TupleClass } from "Tuple/Class"
+import { Class as UndefinedClass } from "Undefined/Class"
+import { Class as UnionClass } from "Union/Class"
+import { Class as UnknownClass } from "Unknown/Class"
+import { Base } from "./Base"
+import { lazy } from "./lazy"
+import { load } from "./load"
+
+export const creator = {
+	any: AnyClass.create,
+	array: ArrayClass.create,
+	boolean: BooleanClass.create,
+	from: FromClass.create,
+	function: FunctionClass.create,
+	null: NullClass.create,
+	number: NumberClass.create,
+	instance: InstanceClass.create,
+	intersection: IntersectionClass.create,
+	object: ObjectClass.create,
+	optional: OptionalClass.create,
+	readonly: ReadonlyClass.create,
+	record: RecordClass.create,
+	string: StringClass.create,
+	tuple: TupleClass.create,
+	undefined: UndefinedClass.create,
+	union: UnionClass.create,
+	unknown: UnknownClass.create,
+	lazy,
+	standard: load,
+}
+Base.isly = creator
---
+++ isly.ts
@@ -0,0 +1,52 @@
+export { creator } from "./creator"
+import { creator } from "./creator"
+
+export { Any } from "./Any"
+export { Array } from "./Array"
+export { Boolean } from "./Boolean"
+export { Class } from "./Class"
+export { Definition } from "./Definition"
+export { Flaw } from "./Flaw"
+export { From } from "./From"
+export { Function } from "./Function"
+export { Instance } from "./Instance"
+export { Intersection } from "./Intersection"
+export { Null } from "./Null"
+export { Number } from "./Number"
+export { islyObject as Object } from "./Object"
+export { Optional } from "./Optional"
+export { Readonly } from "./Readonly"
+export { Record } from "./Record"
+export { Standard } from "./Standard"
+export { String } from "./String"
+export { Tuple } from "./Tuple"
+export { Type } from "./Type"
+export { Undefined } from "./Undefined"
+export { Union } from "./Union"
+export { Unknown } from "./Unknown"
+
+export type Creator = typeof creator
+
+export const any = creator.any
+export const array = creator.array
+export const boolean = creator.boolean
+export const from = creator.from
+const _function = creator.function
+export { _function as function }
+const _null = creator.null
+export { _null as null }
+export const number = creator.number
+export const instance = creator.instance
+export const intersection = creator.intersection
+export const object = creator.object
+export const optional = creator.optional
+export const readonly = creator.readonly
+export const record = creator.record
+export const string = creator.string
+export const tuple = creator.tuple
+const _undefined = creator.undefined
+export { _undefined as undefined }
+export const union = creator.union
+export const unknown = creator.unknown
+export const lazy = creator.lazy
+export const standard = creator.standard
