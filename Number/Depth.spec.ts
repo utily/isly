15c2b24390b1ffc0768532b769798d572be5ba3c
Simon Mika <simon@mika.se>
2025-03-11 11:27:29 +0100

    Updated dependencies and fixed tests.

--- Number/Depth.spec.ts
+++ Number/Depth.spec.ts
@@ -13,17 +13,17 @@ describe("isly.Number.Depth", () => {
 			// eslint-disable-next-line @typescript-eslint/no-unused-vars
 			const data: Depth = value
 		}
-		it.each([
-			[0, false],
-			[1, true],
-			[2, true],
-			[3, true],
-			[4, true],
-			[5, false],
-			[-1, false],
-			[null, false],
-			[undefined, false],
-			["1", false],
-		])("is(%s) == %s", (value, expected) => expect(Depth.is(value)).toEqual(expected))
 	})
+	it.each([
+		[0, false],
+		[1, true],
+		[2, true],
+		[3, true],
+		[4, true],
+		[5, false],
+		[-1, false],
+		[null, false],
+		[undefined, false],
+		["1", false],
+	])("is(%s) == %s", (value, expected) => expect(Depth.is(value)).toEqual(expected))
 })
